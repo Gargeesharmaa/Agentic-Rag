811bf619928e5207dd121f45de43569add5ec8e5
Gargee Sharma <gargeesharma52@gmail.com>
2026-08-05 22:12:23 +0530

    feat: add retriever

---
+++ rag/retrieval.py
@@ -0,0 +1,21 @@
+from typing import List
+from langchain_core.documents import Document
+from vectorstore import VectorStorageManager
+
+class Retriever:
+    """Retrieves the most relevant documents for a user query."""
+    def __init__(self):
+        self.manager = VectorStorageManager()
+        self.vector_searcher = self.manager.load_existing_store()
+
+    def retrieve(
+            self,
+            query: str,
+            top_k: int = 3,
+    ) -> List[Document]:
+        """Retrieves relevant documents using semantic vector search."""
+        documents = self.vector_searcher.similarity_search(
+            query=query,
+            k=top_k
+        )
+        return documents
